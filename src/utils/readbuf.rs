fec9a963f85629445deced98ce58f322a6e4cee3
Francisco Pascoa <francisco.pascoa@usherbrooke.ca>
2024-05-21 14:46:18 -0400

    documented

--- src/utils/readbuf.rs
+++ src/utils/readbuf.rs
@@ -1,9 +1,12 @@
+/// A lightweight reading buffer.
 pub struct ReadBuf<const N: usize> {
     idx: usize,
     buffer: [u8; N]
 }
 
 impl<const N: usize> ReadBuf<{N}> {
+
+    /// Returns a new buffer.
     pub fn new() -> Self {
         let idx = 0;
         let buffer = [0u8; N];
@@ -11,6 +14,7 @@ impl<const N: usize> ReadBuf<{N}> {
         Self {idx, buffer}
     }
 
+    /// Push the given byte to the buffer.
     pub fn push(&mut self, value: u8) -> Result<(), ()> {
         if self.idx < N {
             self.buffer[self.idx] = value;
@@ -21,10 +25,14 @@ impl<const N: usize> ReadBuf<{N}> {
         }
     }
 
+    /// Returns a mutable slice of the buffer up to the last pushed byte.
     pub fn as_mut_slice(&mut self) -> &mut [u8] {
         &mut self.buffer[..self.idx]
     }
 
+    /// Resets the buffer.
+    ///
+    /// It does not overwrite it with zeros, therefore it is really cheap.
     pub fn reset(&mut self) {
         self.idx = 0;
     }
