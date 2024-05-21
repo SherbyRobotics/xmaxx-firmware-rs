37721f1e43015df357a9637df53b8e6643c97525
Francisco Pascoa <francisco.pascoa@usherbrooke.ca>
2024-05-21 14:51:38 -0400

    continued enablement
    works:
    - servo direction
    - driving motors
    - reading from serial
    - writing to serial
    - reading analog pins
    
    todo at least:
    - converting analog readings to speed
    - a proper loop (read command, apply command, send sensors)
    - proper error handling
    - a watchdog

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,10 @@
 #![no_main]
 #![feature(abi_avr_interrupt)]
 
+// use core::ops::RangeInclusive;
+
 use arduino_hal::prelude::*;
+use arduino_hal::simple_pwm::*;
 
 use postcard::{to_slice_cobs, from_bytes_cobs};
 use serde::{Deserialize, Serialize};
@@ -40,43 +43,18 @@ fn main() -> ! {
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
 
-    /*
-     * For examples (and inspiration), head to
-     *
-     *     https://github.com/Rahix/avr-hal/tree/main/examples
-     *
-     * NOTE: Not all examples were ported to all boards!  There is a good chance though, that code
-     * for a different board can be adapted for yours.  The Arduino Uno currently has the most
-     * examples available.
-     */
+    // necessary to make millis() work
+//     millis_init(dp.TC0);
+//     unsafe { avr_device::interrupt::enable() };  // enable interrupts globally
 
+
+    // communication setup
     let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
     let mut read_buf = ReadBuf::<128>::new();
     let mut write_buf = [0u8; 192];
-    let mut led = pins.d13.into_output();
+
     let mut command = Command::default();
-
-    // necessary to make millis() work
-    millis_init(dp.TC0);
-    // Enable interrupts globally
-    unsafe { avr_device::interrupt::enable() };
-
-    loop {
-
-        // read from serial
-        // NOTE for this to work, the send end must send each byte individually.
-        while let Ok(byte) = serial.read() {
-            read_buf.push(byte).unwrap();
-
-            if byte == 0 {
-                command = from_bytes_cobs(read_buf.as_mut_slice()).unwrap();
-                read_buf.reset();
-            }
-        }
-
-
-        // write Sensor to serial
-        let sensors = Sensors {
+    let dummy_sensors = Sensors {
             fl_whl_spd: 0.0,
             fr_whl_spd: 1.0,
             rl_whl_spd: 2.0,
@@ -87,14 +65,74 @@ fn main() -> ! {
             gyro_x: 0.01,
             gyro_y: 0.02,
             gyro_z: 0.03,
-        };
-        if let Ok(msg) = to_slice_cobs(&sensors, &mut write_buf); {
-            for b in msg {
-                nb::block!(serial.write(*b));
-            }
-        }
+    };
 
-        ufmt::uwriteln!(&mut serial, "{}", millis());
+    // steering setup
+//     const STEERING_RANGE_DEG: RangeInclusive<u16> = 35..=135;
+//     const STEERING_RANGE_DUTY: RangeInclusive<u16> = 130..=250;
+    let mut timer1 = Timer1Pwm::new(dp.TC1, Prescaler::Prescale64);
+    let mut steering = pins.d12.into_output().into_pwm(&mut timer1);
+    steering.enable();  // really important
+
+
+    // motors setup TODO uncomment
+    let mut enable_front = pins.d8.into_output();
+    enable_front.set_high();
+    let mut enable_rear = pins.d11.into_output();
+    enable_rear.set_high();
+
+    let timer3 = Timer3Pwm::new(dp.TC3, Prescaler::Prescale64);
+    let timer4 = Timer4Pwm::new(dp.TC4, Prescaler::Prescale64);
+    let mut motor_fl = pins.d2.into_output().into_pwm(&timer3);
+    let mut motor_fr = pins.d3.into_output().into_pwm(&timer3);
+    let mut motor_rl = pins.d5.into_output().into_pwm(&timer3);
+    let mut motor_rr = pins.d6.into_output().into_pwm(&timer4);
+    motor_fl.enable();
+    motor_fr.enable();
+    motor_rl.enable();
+    motor_rr.enable();
+
+    // motor speed sensor setup
+    let mut adc = arduino_hal::Adc::new(dp.ADC, Default::default());
+    let speed_fl = pins.a2.into_analog_input(&mut adc);  // current a0
+    let speed_fr = pins.a3.into_analog_input(&mut adc);  // a1
+    let speed_rl = pins.a6.into_analog_input(&mut adc);  // a4
+    let speed_rr = pins.a7.into_analog_input(&mut adc);  // a5
+
+    let mut led = pins.d13.into_output();
+    led.set_low();
+//     led.enable();
+
+    loop {
+
+        // read from serial
+        // NOTE for this to work, the sending end must send each byte individually.
+        // TODO uncomment
+//         while let Ok(byte) = serial.read() {
+//             read_buf.push(byte).unwrap();
+//
+//             if byte == 0 {
+//                 command = from_bytes_cobs(read_buf.as_mut_slice()).unwrap();
+//                 read_buf.reset();
+//             }
+//         }
+
+        steering.set_duty(250);  // 130..250  mid 190
+        led.toggle();
+        // TODO uncomment
+        motor_fl.set_duty(130);
+        motor_fr.set_duty(130);
+        motor_rl.set_duty(130);
+        motor_rr.set_duty(130);
+
+        // write Sensor to serial TODO uncomment
+//         if let Ok(msg) = to_slice_cobs(&dummy_sensors, &mut write_buf) {
+//             for b in msg {
+//                 nb::block!(serial.write(*b));
+//             }
+//         }
+
+       ufmt::uwriteln!(&mut serial, "{}", speed_fr.analog_read(&mut adc));
         arduino_hal::delay_ms(1000);
     }
 }
